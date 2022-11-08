0c44dc8305501b67e62f9cea9bd1dff2341f4b0b
Diana <diana.wanjiru@student.moringaschool.com>
2022-11-08 12:01:15 +0300

    initial

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < Sinatra::Base
     get '/' do # this is the root route of the application (the homepage) but you can have as many routes as you want
         {hello: "Just a starting code 😃"}.to_json
+        puts "me"
     end
 end
